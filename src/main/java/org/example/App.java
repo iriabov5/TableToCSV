0792934a9a51b94c3ff2511e5c7e2212f3b69f93
Ivan <iriabov5@yandex.ru>
2021-01-11 22:09:22 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args) {
+        // args[0] - это имя базы данных
+        // args[1] - это имя таблицы
+
+        String url = "jdbc:postgresql://localhost:5432/" + args[0];
+        String sql = "SELECT * FROM " + args[1] + ";";
+        Connection c;
+        Statement stmt;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection(url, "postgres", "postgres");
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+            String csv = "/Users/ivan/Desktop/data.csv";
+            CSVWriter writer = new CSVWriter(new FileWriter(csv, false));
+            while (rs.next()) {
+                int columnCount = rs.getMetaData().getColumnCount();
+                String[] strings = new String[columnCount];
+                for (int i = 0; i < columnCount; i++) strings[i] = rs.getString(i + 1);
+                writer.writeNext(strings);
+            }
+            writer.close();
+            rs.close();
+            stmt.close();
+            c.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+}
+
